3041a1199529653ab5076a858a047aa882867112
Alexey Leshchenko <alexey.leshchenko@tantorlabs.ru>
2025-10-29 16:41:39 +0300

    added landing template

--- apps/landing/src/app/app.ts
+++ apps/landing/src/app/app.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
-import { NxWelcome } from './nx-welcome';
+import { FormsModule } from '@angular/forms';
+
+type AddressResult = {
+  id: string;
+  fullAddress: string;
+  country: string;
+};
 
 @Component({
-  imports: [NxWelcome],
+  imports: [FormsModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'landing';
+  query = '';
+  results: AddressResult[] = [];
+
+  onSearch() {
+    // Заглушка: имитируем результаты
+    if (this.query.length > 3) {
+      this.results = [
+        {
+          id: '1',
+          fullAddress: '10 Downing Street, London, SW1A 2AA',
+          country: 'United Kingdom',
+        },
+        {
+          id: '2',
+          fullAddress: '350 5th Ave, New York, NY 10118',
+          country: 'United States',
+        },
+        {
+          id: '3',
+          fullAddress: 'Place de la Concorde, 75008 Paris',
+          country: 'France',
+        },
+        {
+          id: '4',
+          fullAddress: '10 Downing Street, London, SW1A 2AA',
+          country: 'United Kingdom',
+        },
+        {
+          id: '5',
+          fullAddress: '350 5th Ave, New York, NY 10118',
+          country: 'United States',
+        },
+        {
+          id: '6',
+          fullAddress: 'Place de la Concorde, 75008 Paris',
+          country: 'France',
+        },
+      ];
+    } else {
+      this.results = [];
+    }
+  }
 }
